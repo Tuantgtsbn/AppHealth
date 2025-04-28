043f8db9d45e741f24b101f29e3f331a49ee338e
Le Van Thien <levanthienddvtvp01@gmail.com>
2025-04-28 08:24:17 +0700

    change encoding info

--- front_end/app/services/makePrediction.ts
+++ front_end/app/services/makePrediction.ts
@@ -1,61 +1,13 @@
 import scalerParams from '../../assets/ai/scaler_params.json';
 import featureColumns from '../../assets/ai/feature_columns.json';
 import * as tf from '@tensorflow/tfjs';
+import { constructInputVector } from './encodingInfo';
 
 const initTensorflow = async () => {
     await tf.setBackend('cpu');
     await tf.ready();
 };
 
-const parseBloodPressure = (bp: string) => {
-    const [systolic, diastolic] = bp.split('/').map(parseFloat);
-    return { systolic, diastolic };
-};
-
-function createCategoryColumns(userInput: any, featureColumns: string[]) {
-    const categoricalColumns = featureColumns.filter((col) =>
-        col.includes('_')
-    );
-    const categoricalValues = {} as any;
-
-    categoricalColumns.forEach((col) => {
-        const [field, category] = col.split('_');
-        if (userInput[field] === category) {
-            categoricalValues[col] = 1; // Nếu trùng, gán 1
-        } else {
-            categoricalValues[col] = 0; // Nếu không trùng, gán 0
-        }
-    });
-
-    return categoricalValues;
-}
-
-function constructInputVector(userInput: any, featureColumns: any) {
-    const vector = [] as any;
-    const bpParsed = parseBloodPressure(userInput['Blood Pressure']);
-    userInput.Systolic = bpParsed.systolic;
-    userInput.Diastolic = bpParsed.diastolic;
-    const categoricalValues = createCategoryColumns(userInput, featureColumns);
-
-    console.log('categoricalValues:', categoricalValues);
-
-    featureColumns.forEach((col: any) => {
-        if (col in categoricalValues) {
-            vector.push(categoricalValues[col]);
-        } else if (col in userInput) {
-            const userValue = userInput[col];
-            if (typeof userValue === 'number') {
-                vector.push(userValue);
-            } else {
-                vector.push(parseFloat(userValue.toString()) || 0);
-            }
-        } else {
-            vector.push(0);
-        }
-    });
-
-    return vector;
-}
 
 function normalizeInput(vector: number[]) {
     return vector.map((val, idx) => {
@@ -65,7 +17,6 @@ function normalizeInput(vector: number[]) {
     });
 }
 
-// Prediction function
 const makePrediction = async (model: any, userInput: any) => {
     try {
         if (model) {
@@ -75,6 +26,7 @@ const makePrediction = async (model: any, userInput: any) => {
                 userInput,
                 featureColumns
             );
+            console.log('✅ Raw Input Vector: ', rawInputVector);
             const normalizedVector = normalizeInput(rawInputVector);
             console.log('✅ Normalized Input Vector:', normalizedVector);
 
